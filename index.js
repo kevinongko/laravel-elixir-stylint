759e9a68f03ec2ab6ca99f5f6eb35d101c7d6b2d
Kevin Ongko <kevin.ongko@gmail.com>
2016-11-10 10:48:47 +0000

    fix tabbing

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var stylint = require('gulp-stylint');
 var config = elixir.config;
 
 elixir.extend('stylint', function(src, options) {
-	var source = src || config.get('assets.css.stylus.folder') + '/**/*.styl';
+    var source = src || config.get('assets.css.stylus.folder') + '/**/*.styl';
 
 	var paths = new elixir.GulpPaths().src(source);
 
-  new elixir.Task('stylint', function () {
-    return gulp.src(paths.src.path)
-			.pipe(stylint(options))
-			.pipe(stylint.reporter())
-      .pipe(stylint.reporter('fail', { failOnWarning: true }))
-			.on('error', this.onError());
-  })
+    new elixir.Task('stylint', function () {
+        return gulp.src(paths.src.path)
+        .pipe(stylint(options))
+		.pipe(stylint.reporter())
+        .pipe(stylint.reporter('fail', { failOnWarning: true }))
+		.on('error', this.onError());
+    })
 	.watch(paths.src.path);
 });
