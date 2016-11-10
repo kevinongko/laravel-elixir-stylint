04ea2b102d7e5f20e992673caa0ba230c7da4c7c
Kevin Ongko <kevin.ongko@gmail.com>
2016-11-10 15:53:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var Elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var stylint = require('gulp-stylint');
+
+var config = Elixir.config;
+
+Elixir.extend('stylint', function(src, options) {
+	var source = src || config.get('assets.stylus.folder') + '/**/*.styl';
+
+	var paths = new Elixir.GulpPaths().src(source);
+
+  var onError = function (err) {
+		new Elixir.Notification().error(err, 'Stylus Lint failed!');
+		this.emit('end');
+	};
+
+  new elixir.Task('stylint', function () {
+		this.log(paths.src);
+
+    return gulp.src(paths.src.path)
+			.pipe(stylint(options))
+			.pipe(stylint.reporter())
+			.on('error', onError);
+			.pipe(new Elixir.Notification('Styl Lint succeeded!'))
+  })
+	.watch(paths.src.path);
+});
