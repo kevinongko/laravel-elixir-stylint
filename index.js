348777ae746ae5c9cc6e1aebf4d701a12556d85e
Kevin Ongko <kevin.ongko@gmail.com>
2016-11-10 10:49:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var config = elixir.config;
 elixir.extend('stylint', function(src, options) {
     var source = src || config.get('assets.css.stylus.folder') + '/**/*.styl';
 
-	var paths = new elixir.GulpPaths().src(source);
+    var paths = new elixir.GulpPaths().src(source);
 
     new elixir.Task('stylint', function () {
         return gulp.src(paths.src.path)
-        .pipe(stylint(options))
-		.pipe(stylint.reporter())
-        .pipe(stylint.reporter('fail', { failOnWarning: true }))
-		.on('error', this.onError());
+            .pipe(stylint(options))
+            .pipe(stylint.reporter())
+            .pipe(stylint.reporter('fail', { failOnWarning: true }))
+            .on('error', this.onError());
     })
-	.watch(paths.src.path);
+    .watch(paths.src.path);
 });
